5f1a407fd352c6b815b914a9dbca71fa217b537d
Chris <christophe.barreto@gmail.com>
2014-04-02 08:39:39 +0200

    Parser Jackson

--- config.js
+++ config.js
@@ -5,14 +5,15 @@
     "workers" : [
         '10.66.1.2',
         '10.66.1.3:45678'
-    ]
+                ]
     "headers" : {
         "incoming": {
             "add" : [{"My-ProxyHeader" : "123"}]
-        },
+                    },
+
         "outgoing" : {
             "add" : [{"For-Client-Header" : "765"}]
-        }
-    }
-}]
+                     }
+                }
+            }]
 }
